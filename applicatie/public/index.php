c3fc4ec011a007317bd7c16352a6414a3f326113
MarsTwix <marnixwildeman@gmail.com>
2020-12-18 10:02:42 +0100

    added db_connections fix

--- applicatie/public/index.php
+++ applicatie/public/index.php
@@ -1,24 +1,5 @@
 <?php
-<<<<<<< Updated upstream
-  print_r(PDO::getAvailableDrivers());
-
-  // Naam van server
-$hostname = 'host.docker.internal';
-// Naam van database
-$dbname = 'FLETNIX_DOCENT';
-// Hier je eigen gebruikersnaam
-$username = 'application';
-// Hier je eigen password.
-// Zet het wachtwoord in het echt nooit letterlijk in de broncode.
-$pw = '1234';
-
-// Connectie met de database ofwel de Database Handler (dbh).
-$dbh = new PDO("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", $username, $pw);
-// Tijdens het ontwikkelen is het handig om meteen ook de foutmeldingen vanuit de database te kunnen lezen.
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-=======
-require_once php/db_connectie.php;
->>>>>>> Stashed changes
+require_once 'php/db_connectie.php';
 
 // Een query net zoals we dat wel vaker in SQL doen.
 $query = <<<EOD
