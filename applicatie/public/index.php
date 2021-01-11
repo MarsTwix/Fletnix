dbfdd2a82b305438c00c432d0425d4567954f682
MarsTwix <marnixwildeman@gmail.com>
2021-01-11 14:43:27 +0100

    Added docker

---
+++ applicatie/public/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$charactersLength = strlen($characters);
+$randomString = '';
+for ($i = 0; $i < 10; $i++) {
+    $randomString .= $characters[rand(0, $charactersLength - 1)];
+}
+echo "<br> random code: {$randomString} <br>"
+
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+  <a href='homepage.html'>films.php</a> 
+  <a href='databaseUsersTest.php'>databaseUsersTest</a>
+  </body>
+</html>
+
