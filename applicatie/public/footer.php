daab8978f7cf79dff21adccc9da3c8c52717173b
Suikerbuik <74355762+Suikerbuik@users.noreply.github.com>
2021-01-12 15:19:16 +0100

    responsive

--- applicatie/public/footer.php
+++ applicatie/public/footer.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html>
 
-
+<head>
+    <title>Fletnix - Home</title>
+    <link rel="stylesheet" href="style/normalize.css">
+    <link rel="stylesheet" href="style/main.css">
+    <link rel="icon" href="img/logo.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
 
 <body ></body>
 
