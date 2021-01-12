7a34f3b7033f870b34107bb835c4ac899ed9a9b2
MarsTwix <marnixwildeman@gmail.com>
2021-01-12 11:49:27 +0100

    Old account file

--- applicatie/public/account.php
+++ applicatie/public/account.php
@@ -1,13 +1,8 @@
 <?php
 
-    if (!empty($_POST['newMail']) && !empty($_POST['newMail2'])) {
-        if ($_POST['newMail'] = $_POST['newMail2']) {
-            if (!compareEmail($_POST['newMail'])) {
-                alterUserData("customer_mail_address", $SESSION['email'], $_POST['newMail']);
-            }
-        }
-    }
-
+    require_once "php/data_functions.php";
+    session_start();
+    $customer = getCustomerData($_SESSION['email']);
 ?>
 
 <!doctype html>
@@ -15,29 +10,26 @@
 <html>
 
 <head>
-    <title>Fletnix - Account detail</title>
+<title>Fletnix - Account</title>
     <link rel="stylesheet" href="style/normalize.css">
     <link rel="stylesheet" href="style/main.css">
     <link rel="icon" href="img/logo.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
-
 <body class="filmbg bg">
     <nav class="back">
-        <a class="buttonlink" href="filmoverzicht.html">BACK</a>
+<a class="buttonlink" href="filmoverzicht.html">BACK</a>
     </nav>
-
-
-        <form action="databaseUsersTest.php" method="post">
-        <input name="newMail" type="text" placeholder="Email">
-        <input type="newMail2" placeholder="Email opnieuw">
-        <input type="submit">
-        <a class="buttonlink" href = "accountdetail.html">wijzigen</a>
-        </form>
+<main class="account">
+        <div class="centertext">
+            <?=$customer['customer_mail_address']?>
+            </div>
+            <div class="centertext link">
+            <a class="buttonlink" href="account.html" >Email wijzigen</a>
+            </div>
+            <div class="centertext link">
+            <a class="buttonlink" href="Wachtwoord_wijzigen.html" >Wachtwoord wijzigen</a>
+        </div>
 
     </main>
-
 </body>
-
-
-</html>
--- applicatie/public/email_wijzigen.php
+++ applicatie/public/email_wijzigen.php
@@ -1,5 +1,13 @@
 <?php
-    
+
+    if (!empty($_POST['newMail']) && !empty($_POST['newMail2'])) {
+        if ($_POST['newMail'] = $_POST['newMail2']) {
+            if (!compareEmail($_POST['newMail'])) {
+                alterUserData("customer_mail_address", $SESSION['email'], $_POST['newMail']);
+            }
+        }
+    }
+
 ?>
 
 <!doctype html>
@@ -7,7 +15,7 @@
 <html>
 
 <head>
-    <title>Fletnix - Email wijzigen</title>
+    <title>Fletnix - Account detail</title>
     <link rel="stylesheet" href="style/normalize.css">
     <link rel="stylesheet" href="style/main.css">
     <link rel="icon" href="img/logo.png">
@@ -16,17 +24,20 @@
 
 <body class="filmbg bg">
     <nav class="back">
-        <a class="buttonlink" href="accountdetail.html">BACK</a>
+        <a class="buttonlink" href="filmoverzicht.html">BACK</a>
     </nav>
 
-    <main class="center-screen filmbg bg">
-        <h2 class="centertext">Email wijzigen</h2>
 
-        <input type="email" placeholder="Email">
-        <input type="email" placeholder="Email opnieuw">
+        <form action="databaseUsersTest.php" method="post">
+        <input name="newMail" type="text" placeholder="Email">
+        <input type="newMail2" placeholder="Email opnieuw">
+        <input type="submit">
         <a class="buttonlink" href = "accountdetail.html">wijzigen</a>
-        
+        </form>
+
     </main>
+
 </body>
+
 
 </html>
