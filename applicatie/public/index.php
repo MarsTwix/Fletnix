4c2ed3f238e9cf0c459f02593ead2822f4701d4f
Reomar <daniel.groenendijk.dg@gmail.com>
2021-01-14 11:02:23 +0100

    Update index.php

--- applicatie/public/index.php
+++ applicatie/public/index.php
@@ -2,36 +2,36 @@
 require_once 'php/db_connectie.php';
 // Een query net zoals we dat wel vaker in SQL doen.
 
-$query = <<<EOD
-DROP TABLE IF EXISTS fletnix_user;
-CREATE TABLE fletnix_user (
-    userID INT PRIMARY KEY IDENTITY(1,1),
-    username VARCHAR(100) NOT NULL UNIQUE
-);
+// $query = <<<EOD
+// DROP TABLE IF EXISTS fletnix_user;
+// CREATE TABLE fletnix_user (
+//     userID INT PRIMARY KEY IDENTITY(1,1),
+//     username VARCHAR(100) NOT NULL UNIQUE
+// );
 
-INSERT INTO fletnix_user (username)
-VALUES ('student')
-EOD;
-// Query uitvoeren.
-$result = $dbh->exec($query);
+// INSERT INTO fletnix_user (username)
+// VALUES ('student')
+// EOD;
+// // Query uitvoeren.
+// $result = $dbh->exec($query);
 
-// Alle users ophalen.
-$users = $dbh->query('SELECT * from fletnix_user');
+// // Alle users ophalen.
+// $users = $dbh->query('SELECT * from fletnix_user');
 
-//Resultaten per rij printen.
-foreach($users as $row) {
-  print_r($row);
-}
+// //Resultaten per rij printen.
+// foreach($users as $row) {
+//   print_r($row);
+// }
 
-$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-$charactersLength = strlen($characters);
-$randomString = '';
-for ($i = 0; $i < 10; $i++) {
-    $randomString .= $characters[rand(0, $charactersLength - 1)];
-}
-echo "<br> random code: {$randomString} <br>";
+// $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// $charactersLength = strlen($characters);
+// $randomString = '';
+// for ($i = 0; $i < 10; $i++) {
+//     $randomString .= $characters[rand(0, $charactersLength - 1)];
+// }
+// echo "<br> random code: {$randomString} <br>";
 
-header("Location: pages.php");
+header("Location: filmoverzicht_preview.php");
 
 ?>
 
