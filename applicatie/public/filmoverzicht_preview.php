e95763484464cbe0250ae3e346957289dccb744e
Suikerbuik <74355762+Suikerbuik@users.noreply.github.com>
2021-01-14 10:51:26 +0100

    films update

--- applicatie/public/filmoverzicht_preview.php
+++ applicatie/public/filmoverzicht_preview.php
@@ -29,12 +29,12 @@
 
         <a class="films"> <img src="img/the_minions.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/Brandweer.jpg"  height="180" alt="no img"> </a>
-        <a class="films"  href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> <img src="img/bee_movie.jpg"  height="180" alt="no img"> </a>
-        <a class="films" href="https://www.youtube.com/watch?v=o9aNP_I0E40"> <img src="img/frozen.jpg"  height="180" alt="no img"> </a>
+        <a class="films"> <img src="img/bee_movie.jpg"  height="180" alt="no img"> </a>
+        <a class="films"> <img src="img/frozen.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/UP.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/Cars.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/Avatarrr.jpg"  height="180" alt="no img"> </a>
-        <a class="films" href="https://www.youtube.com/watch?v=o9aNP_I0E40"> <img src="img/Pingu.jpg"  height="180" alt="no img"> </a>
+        <a class="films"> <img src="img/Pingu.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/Lion_king.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/Lego.jpg"  height="180" alt="no img"> </a>
         <a class="films">  <img src="img/Smurven.jpg"  height="180" alt="no img"> </a>
@@ -50,12 +50,12 @@
 
         <a class="films"> <img src="img/the_minions.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/Brandweer.jpg"  height="180" alt="no img"> </a>
-        <a class="films"  href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"> <img src="img/bee_movie.jpg"  height="180" alt="no img"> </a>
-        <a class="films" href="https://www.youtube.com/watch?v=o9aNP_I0E40"> <img src="img/frozen.jpg"  height="180" alt="no img"> </a>
+        <a class="films"> <img src="img/bee_movie.jpg"  height="180" alt="no img"> </a>
+        <a class="films"> <img src="img/frozen.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/UP.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/Cars.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/Avatarrr.jpg"  height="180" alt="no img"> </a>
-        <a class="films" href="https://www.youtube.com/watch?v=o9aNP_I0E40"> <img src="img/Pingu.jpg"  height="180" alt="no img"> </a>
+        <a class="films"> <img src="img/Pingu.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/Lion_king.jpg"  height="180" alt="no img"> </a>
         <a class="films"> <img src="img/Lego.jpg"  height="180" alt="no img"> </a>
         <a class="films">  <img src="img/Smurven.jpg"  height="180" alt="no img"> </a>
